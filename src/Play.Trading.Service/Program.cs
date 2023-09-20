44a1dcbc3d28a23055bd9c449c78fd54db7fb61a
Son Quang Pham (SM) <sonpham@deloitte.ca>
2023-09-20 13:54:59 +0700

    Configure KeyVault

--- src/Play.Trading.Service/Program.cs
+++ src/Play.Trading.Service/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Play.Common.Configurations;
 
 namespace Play.Trading.Service
 {
@@ -18,6 +19,7 @@ namespace Play.Trading.Service
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAzureKeyVault()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
