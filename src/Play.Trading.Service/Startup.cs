b34d0ed5f157cb572e215d3e7497e48073872a83
SonPham <phamquangson1994@gmail.com>
2023-10-17 17:52:14 +0700

    Switch to Common library extension for logging

--- src/Play.Trading.Service/Startup.cs
+++ src/Play.Trading.Service/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Play.Common.HealthChecks;
 using Play.Common.Identity;
+using Play.Common.Logging;
 using Play.Common.MassTransit;
 using Play.Common.MongoDB;
 using Play.Common.Settings;
@@ -70,11 +71,7 @@ namespace Play.Trading.Service
             services.AddHealthChecks()
                 .AddMongoDb();
 
-            services.AddLogging(loggingBuilder =>
-            {
-                var seqSettings = Configuration.GetSection(nameof(SeqSettings)).Get<SeqSettings>();
-                loggingBuilder.AddSeq(serverUrl: seqSettings.ServerUrl);
-            });
+            services.AddSeqLogging(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
