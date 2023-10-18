469603b2ec4c9799bf60dc457a0ead2dd7c21865
Son Quang Pham (SM) <sonpham@deloitte.ca>
2023-10-18 17:55:49 +0700

    update

--- src/Play.Trading.Service/Startup.cs
+++ src/Play.Trading.Service/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Play.Common.HealthChecks;
 using Play.Common.Identity;
 using Play.Common.Logging;
@@ -71,6 +73,37 @@ namespace Play.Trading.Service
                 .AddMongoDb();
 
             services.AddSeqLogging(Configuration);
+
+            services.AddOpenTelemetryTracing(builder => 
+            {
+                var serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+                //this means that we want to collect information from a anything that's immediate directly by our microservice. In this case, this is the trading
+                //microservice
+                builder.AddSource(serviceSettings.ServiceName)
+
+                // Because masstransit already has a bunch of in instrumentation build in that can tell give us information about messages that have been received.
+                // The message have been sent, published, and a bunch of other information that's already been immediate by mass transit. We want to make sure that
+                // we also collect that information. And for that we have to use identified for mass transit
+                .AddSource("MassTransit")
+
+                // define or give a name to the trading microservice as a resource within the long tail of traces that are going to be showing up and to do that.
+                // We want to use the method called SetResourceBuilder.
+                .SetResourceBuilder(
+                    ResourceBuilder.CreateDefault()
+                        // we identify our service in the long list of choices that are going to be showing up.
+                        .AddService(serviceName: serviceSettings.ServiceName))
+
+                //Add the additional instrumentation that we want to be collecting here. This allows us to track Http calls that come from our microservice to 
+                // the outside, basically any Http requests that come from microservice to the outside.
+                .AddHttpClientInstrumentation()
+
+                // this allows us to track any inbound request into our controllers right into our microservice via APIs.
+                .AddAspNetCoreInstrumentation()
+                
+                //where we want to export this tracing information. so that we're going to be using the console exporter right now.
+                .AddConsoleExporter();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
